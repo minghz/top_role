e0e50de6080866f345e76f44d5557bbac7cfc0e8
Minghua Zhao <minghz42@gmail.com>
2019-07-19 23:39:16 -0400

    Use owner_id for GameRoom

--- test/top_role_web/schemas/game_room_schema_test.exs
+++ test/top_role_web/schemas/game_room_schema_test.exs
@@ -1,17 +1,17 @@
 defmodule TopRoleWeb.GameRoomSchemaTest do
   use TopRoleWeb.ConnCase
 
-  test "game_room must have a user_id" do
+  test "game_room must have a owner_id" do
     changeset = TopRole.GameRoom.changeset(%TopRole.GameRoom{}, %{})
 
-    assert changeset.errors == [user_id: {"can't be blank", [validation: :required]}]
+    assert changeset.errors == [owner_id: {"can't be blank", [validation: :required]}]
   end
   
   test "user has many game_rooms" do
     user = TopRole.Repo.insert!(%TopRole.User{email: "dummy@example.com"})
 
-    TopRole.Repo.insert!(%TopRole.GameRoom{user_id: user.id, description: "game room 1"})
-    TopRole.Repo.insert!(%TopRole.GameRoom{user_id: user.id, description: "game room 2"})
+    TopRole.Repo.insert!(%TopRole.GameRoom{owner_id: user.id, description: "game room 1"})
+    TopRole.Repo.insert!(%TopRole.GameRoom{owner_id: user.id, description: "game room 2"})
 
     game_rooms = TopRole.Repo.all Ecto.assoc(user, :game_rooms)
 
@@ -20,9 +20,9 @@ defmodule TopRoleWeb.GameRoomSchemaTest do
 
   test "game_room belongs to a user" do
     user = TopRole.Repo.insert!(%TopRole.User{email: "dummy@example.com"})
-    game_room = TopRole.Repo.insert!(%TopRole.GameRoom{user_id: user.id, description: "game room 1"})
+    game_room = TopRole.Repo.insert!(%TopRole.GameRoom{owner_id: user.id, description: "game room 1"})
 
-    associated_user = TopRole.Repo.one! Ecto.assoc(game_room, :user)
+    associated_user = TopRole.Repo.one! Ecto.assoc(game_room, :owner)
 
     assert associated_user == user
   end
